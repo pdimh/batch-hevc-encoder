9e449498e97817e069418bf1beae0735ee4831a3
Pedro Henrique da Silva Palhares <phpalhares@gmail.com>
2023-07-13 18:22:52 -0300

    Improve output messages

--- batchhevcencoder.sh
+++ batchhevcencoder.sh
@@ -4,10 +4,11 @@
 # Author: Pedro Henrique da Silva Palhares
 
 crf=23
-output_folder="output/"
+output_folder="output"
 preset="medium"
+verbose="-loglevel error -x265-params log-level=none"
 
-# Function to print help message
+# Print help message
 print_help() {
   echo "Perform batch encoding of videos to HEVC using ffmpeg."
   echo
@@ -15,20 +16,49 @@ print_help() {
   echo "  -h                          Print this help."
   echo "  -f <crf>                    Set the Constant Rate Factor. Default: $crf"
   echo "  -p <preset>                 Set the preset. Default: $preset"
-  echo "  -o <output>                 Set the output folder. Default: $output_folder"
+  echo "  -o <output>                 Set the output folder. Default: $output_folder/"
+  echo "  -v                          Enable verbose of ffmpeg."
   echo
 }
 
-convert() {
+print_info() {
+  local BLUE='\e[1;94m'
+  local YELLOW='\e[0;93m'
+  local NC='\e[0m'
+  echo -e "[${BLUE}INFO${NC}] ${YELLOW}$@${NC}"
+}
+
+print_err() {
+  local RED='\e[1;31m'
+  local LRED='\e[0;91m'
+  local YELLOW='\e[0;93m'
+  local NC='\e[0m'
+  echo -e "[${RED}ERROR${NC}] ${LRED}$@${NC}" >&2
+}
+
+print_success() {
+  local BLUE='\e[1;94m'
+  local GREEN='\e[1;92m'
+  local NC='\e[0m'
+  echo -e "[${BLUE}INFO${NC}] ${GREEN}$@${NC}"
+}
+
+encode() {
   local filename=$1
   local crf=$2
   local preset=$3
   local output_file=$4
 
-  ffmpeg -i "$filename" -c:v libx265 -crf $crf -preset $preset -c:a copy -n "$output_file"
+  print_info "Encoding $filename..."
+  ffmpeg -i "$filename" -c:v libx265 -crf $crf -preset $preset -c:a copy $verbose -n "$output_file"
+  if [[ $? -ne "0" ]]; then
+    print_err "Error encoding $filename"
+    rm $output_file
+    exit 1
+  fi
 }
 
-while getopts ": h f: o: p:" opt; do
+while getopts ": h f: o: p: v" opt; do
   case $opt in
     h)
       print_help
@@ -42,6 +72,9 @@ while getopts ": h f: o: p:" opt; do
       ;;
     p)
       preset="$OPTARG"
+      ;;
+    v)
+      verbose=""
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -75,20 +108,19 @@ for f in *; do
 
   check_vid=$(echo "$probe" | grep "codec_type=video")
   check_hevc=$(echo "$probe" | grep "codec_name=hevc")
-  
-  echo "$fname $check_vid $check_hevc"
 
   if [[ ! -z $check_vid && -z $check_hevc ]]; then 
     new_fname="$output_folder/${fname}_x265.mp4"
 
     if [[ ! -e $new_fname ]]
     then
-      convert $f $crf $preset $new_fname
+      encode $f $crf $preset $new_fname
     else
-      echo "OK"
+      print_info "$new_fname already exists."
     fi
   elif [[ ! -z $check_vid ]]; then
     [[ $fname == *"x265" ]] && new_fname="$output_folder/$f" || new_fname="$output_folder/${fname}_x265.$ext"
     [ -e $new_fname ] || cp -n "$f" "$new_fname"
   fi
 done
+print_success "Finished! Check if all files were correctly encoded."
