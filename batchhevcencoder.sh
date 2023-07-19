f2cd5cd494251b2ca58c332960fdd3954f27b7c1
Pedro Henrique da Silva Palhares <phpalhares@gmail.com>
2023-07-19 20:24:32 -0300

    Add GPL v2 header

--- batchhevcencoder.sh
+++ batchhevcencoder.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-# Perform batch encoding of videos to HEVC using ffmpeg
-# Author: Pedro Henrique da Silva Palhares
+# Perform batch encoding of videos to HEVC using FFmpeg
+# Copyright (C) 2023  Pedro Henrique da Silva Palhares
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 crf=23
 output_folder="output"
@@ -10,14 +23,14 @@ verbose="-loglevel error -x265-params log-level=none"
 
 # Print help message
 print_help() {
-  echo "Perform batch encoding of videos to HEVC using ffmpeg."
+  echo "Perform batch encoding of videos to HEVC using FFmpeg."
   echo
-  echo "Sintax: batchhevcencoder.sh [-h|-?] [-f <crf>] [-o <output>] [-p <preset>]"
+  echo "Sintax: batchhevcencoder.sh [-h] [-f <crf>] [-o <output>] [-p <preset>]"
   echo "  -h                          Print this help."
   echo "  -f <crf>                    Set the Constant Rate Factor. Default: $crf"
   echo "  -p <preset>                 Set the preset. Default: $preset"
   echo "  -o <output>                 Set the output folder. Default: $output_folder/"
-  echo "  -v                          Enable verbose of ffmpeg."
+  echo "  -v                          Enable verbose of FFmpeg."
   echo
 }
 
